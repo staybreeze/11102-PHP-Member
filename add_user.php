6580a46123281846dd2819ce59cee9c10e0937e3
staybreeze <wunshengliao@gmail.com>
2023-11-13 13:12:22 +0800

    新增註冊頁面

---
+++ add_user.php
@@ -0,0 +1,9 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=member";
+$pdo=new PDO($dsn,'root','');
+
+$sql="insert into `users`(`acc`,`pw`,`name`,`address`,`email`)
+                    values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['name']}','{$_POST['address']}','{$_POST['email']}')";
+
+
+                    $pdo->exec($sql);
